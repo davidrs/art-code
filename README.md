59c75d7005459df30fbe4dccf9e0cb695ec37bc8
davidrs <drustsmith@drustsmith-macbookpro.roam.corp.google.com>
2016-03-15 17:25:23 -0700

    Add timeline with hash to toggle it on and off. And improved timing to map to real time

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # art-code
 Fun art projects, seeded with data
+
+
+http://rawgit.com/davidrs/art-code/master/alcohol-licenses/index.html#timeline
+
+or 
+
+http://rawgit.com/davidrs/art-code/master/alcohol-licenses/index.html
--- alcohol-licenses/map.js
+++ alcohol-licenses/map.js
@@ -10,8 +10,6 @@ var svgContainer = d3.select("body").append("svg")
                                      .attr("width", 1000)
                                      .attr("height", 700);
 
-
-
 var svg = d3.select("svg");
 
 
@@ -28,60 +26,78 @@ var loadCSVs = function(){
 		    if(a.Orig_Iss_D > b.Orig_Iss_D) return 1;
 		    return 0;
 		});
+		rows = rows.filter(function(d){
+			return d['License_Ty'] < 22 
+		});
+		console.log(rows.length);
+		console.log(rows[0], rows[23])
 		var multiplier = 2400;
 		var color = d3.scale.category20();
+		firstYear = rows[0].Orig_Iss_D.split('/')[0];
+		console.log(firstYear);
+		//console.log(firstYear);
 		svg.selectAll("circle")
 		    .data(rows)
 		  .enter()
 	      .append("circle")
 	      // Filter to license types.
-	      .filter(function(d) { return d['License_Ty'] < 22 })
-		    .attr("cx", function(d) { console.log(d.Orig_Iss_D); return ((-d.X *10000) % 2) < 1?0:1000; })
+		    .attr("cx", function(d) { return ((-d.X *10000) % 2) < 1?0:1000; })
 		    .attr("r", function(d) { return 0;})
 		    .attr("cy", function(d) {return  ((d.Y)*multiplier) % 2 < 1?0 : 1000; })
 
-		  	.transition().duration(9000)//function(d){return d.X*50;})
-			.delay(function(d,i){return i*50;})
- 			.attr("fill",function(d,i){return color(d.zip);})
+		  	.transition().duration(function(d,i){return Math.min(9000, 4000+i*200)})//function(d){return d.X*50;})
+			.delay(function(d,i){	
+				return getDelay(d,i);
+				})// off of year.
+				.attr("fill",function(d,i){return color(d.zip);})
 		    .attr("cy", function(d) { return  ((d.Y)*multiplier) % multiplier - 1600; })
 		    .attr("cx", function(d) { return -1* ((d.X)*multiplier) % multiplier - 700; })
 		    .attr("r", function(d) { return 2;});
 
-		//drawLine(rows);
+		if(window.location.hash.indexOf("timeline")>=0){
+			drawLine(rows, firstYear);
+		}
 	});
 }
 
-var drawLine = function(rows){
-		    //Add the SVG Text Element to the svgContainer
-			var text = svgContainer.selectAll("text")
-	                        .data(rows)
-	                        .enter()
- 	                        .append("text")
-	      					.filter(function(d) { return d['License_Ty'] < 22 });
-	
-			//Add SVG Text Element Attributes
-			/*
-			var textLabel = svgContainer.selectAll("text")
-	                 .attr("x", function(d,i){return i*30})
-	                 .attr("y", 20)
-	                 .text('text')
-	                 .attr("font-family", "sans-serif")
-	                 .attr("font-size", "15px")
-	                 .text( function (d) { return d.Orig_Iss_D; })
-				     .style("opacity", 0)	         
-	                 .transition().duration(9000).delay(function(d,i){return i*1000;})
-    				.style("opacity", 1)
+var getDelay = function(d,i){
+	var year = +d.Orig_Iss_D.split('/')[0];
+	//	console.log(year-firstYear);
+	return (year-firstYear) * (year<1976?500+20*i:1000) + (+d.Orig_Iss_D.split('/')[1])*30
+}
+
+var drawLine = function(rows, firstYear){
+	    //Add the SVG Text Element to the svgContainer
+		var text = svgContainer.selectAll("text")
+                        .data(rows)
+                        .enter()
+	                    .append("text");
+
+		//Add SVG Text Element Attributes
+		/*
+		var textLabel = svgContainer.selectAll("text")
+                 .attr("x", function(d,i){return i*30})
+                 .attr("y", 20)
+                 .text('text')
+                 .attr("font-family", "sans-serif")
+                 .attr("font-size", "15px")
+                 .text( function (d) { return d.Orig_Iss_D; })
+			     .style("opacity", 0)	         
+                 .transition().duration(9000).delay(function(d,i){return i*1000;})
+				.style("opacity", 1)
     				*/
-//TODO add data filter
-	    var line = d3.svg.line()
-      .interpolate("cardinal")	      
-      .x(function(d,i) {return i*3/4;})
-      .y(function(d) {return 5*Math.random();})
+
+	var line = d3.svg.line()
+      .interpolate("cardinal")	   
+      .x(function(d,i) {
+      	var multiplier = 9
+      	return 36 +(+d.Orig_Iss_D.split('/')[0]- firstYear) * multiplier +d.Orig_Iss_D.split('/')[1]*multiplier/12;})
+      .y(function(d,i) {return 69 - i * 60.0/rows.length})
 
     var path = svg.append("path")
       .attr("d", line(rows))
       .attr("stroke", "steelblue")
-      .attr("stroke-width", "2")
+      .attr("stroke-width", "1")
       .attr("fill", "none");
 
     var totalLength = path.node().getTotalLength();
@@ -90,7 +106,7 @@ var drawLine = function(rows){
       .attr("stroke-dasharray", totalLength + " " + totalLength)
       .attr("stroke-dashoffset", totalLength)
       .transition()
-        .duration(rows.length *50+9000)
+        .duration(rows.length * 30 + (2013 - firstYear) * 800)
         .ease("linear")
         .attr("stroke-dashoffset", 0);
 
@@ -98,59 +114,18 @@ var drawLine = function(rows){
 		//TODO add axis line and text, line grows with tinme.
 		//Add the SVG Text Element to the svgContainer
 		var text = svgContainer.append('text')
-                 .attr("x", 10)
-                 .attr("y", 20)
-                 .text(rows[0].Orig_Iss_D);
+                 .attr("x", 13)
+                 .attr("y", 68)
+	             .attr("font-family", "sans-serif")
+      			 .attr("fill", "steelblue")
+	             .attr("font-size", "15px")
+                 .text(firstYear);
 		var text = svgContainer.append('text')
-                 .attr("x", 500)
+                 .attr("x", 590)
                  .attr("y", 20)
-                 .text(rows[rows.length-1].Orig_Iss_D);
+      			 .attr("fill", "steelblue")
+                 .attr("font-family", "sans-serif")
+                 .attr("font-size", "15px")
+                 .text(rows[rows.length-1].Orig_Iss_D.split('/')[0]);
 
 };
-
-// Determines colour of map polygons.
-var getColor = function(d) {
-	return d > 1.5 ? '#800012' :
-	       d >= 1.01  ? '#FC532A' :
-	       d >= 1.0  ? '#FDFD3C' :
-	       d > 0.5   ? '#D9FE76' :
-	       d >= 0.0   ? '#B2FE4C' :
-	                  '#EEE';
-}
-
-
-var resetHighlight = function(e) {
-	geojson.resetStyle(e.target);
-	info.update();
-}
-
-var zoomToFeature = function(e) {
-	map.fitBounds(e.target.getBounds());
-}
-
-
-var onEachFeature = function(feature, layer) {
-	var censusTract = +feature.properties.TRACT;
-	feature.properties.censusTract = censusTract;
-	layer.on({
-		mouseover: highlightFeature,
-		mouseout: resetHighlight,
-		click: zoomToFeature
-	});
-}
-
-// Pretty round a # to 2 decimal digits.
-var prettyRound = function(number){
-	return Math.round(number * 100);
-}
-
-// Get the ratio of actual to quota for a specific census tract.
-var getRatio = function(censusTract, label){
-	return (combinedData[censusTract][label].actual / combinedData[censusTract][label].quota);
-}
-
-// Get the difference between the quote and actual # of licenses for a specific census tract.
-var getDelta = function(censusTract, label){
-	return (combinedData[censusTract][label].quota - combinedData[censusTract][label].actual);
-}
-
