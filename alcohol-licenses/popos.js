6147a7c52c2ccaf3292345dcac76ce13881d3f7e
davidrs <drustsmith@drustsmith-macbookpro.roam.corp.google.com>
2016-03-24 18:37:00 -0700

    fix city orientation

--- alcohol-licenses/popos.js
+++ alcohol-licenses/popos.js
@@ -1,6 +1,5 @@
-var LICENSES_CSV = "data/alcohol_locations.csv";
-//var LICENSES_CSV = "data/poposStripped.csv";
 
+var CSV = "data/poposStripped.csv";
 
 // Leaflet map object.
 var map;
@@ -19,7 +18,7 @@ var global_delay = 500;
 
 // Setup the leaflet map and legend;
 var setupMap = function(){
-	drawTitle();	
+	drawTitle();
 	loadCSVs();
 
 	if(window.location.hash.indexOf("fast")>=0){
@@ -29,29 +28,34 @@ var setupMap = function(){
 
 
 var loadCSVs = function(){
-	d3.csv(LICENSES_CSV, function(rows){
+	d3.csv(CSV, function(rows){
 		rows = rows.sort(function(a,b) {
 		    if(a.Orig_Iss_D < b.Orig_Iss_D) return -1;
 		    if(a.Orig_Iss_D > b.Orig_Iss_D) return 1;
 		    return 0;
 		});
 		rows = rows.filter(function(d){
-			var tmp= d.X;
-			d.X=d.X;
-			d.Y= -d.Y;//tmp;
-			return  d['License_Ty'] < 22 ;
+			var tmp= -d.X;
+			d.X= d.X;
+			d.Y= -d.Y;
+			if (d['License_Ty']){
+				return d['License_Ty'] < 22 ;
+			}else{
+				return true;
+			}
 		});
+
 		devRows = rows; // intentional global for row debugging.
 		console.log(rows.length);
 		console.log(rows[0], rows[23]);
 		var xScale = d3.scale.linear()
                      .domain([d3.min(rows, function(d) { return d.X; })
                      	, d3.max(rows, function(d) { return d.X; })])
-                     .range([ width - margin, margin]);
+                     .range([width - margin, margin]);
 		var yScale = d3.scale.linear()
                      .domain([d3.min(rows, function(d) { return d.Y; }), 
                      	d3.max(rows, function(d) { return d.Y; })])
-                     .range([0, height-margin]);
+                     .range([margin, height-margin]);
 
 		var multiplier = 2400;
 		var color = d3.scale.category20();
@@ -62,18 +66,16 @@ var loadCSVs = function(){
 		    .data(rows)
 		  .enter()
 	      .append("circle")
-		    .attr("cx", function(d) { return  ((d.X)*multiplier) % 2 < 1 ? 0 : 1000; })
-		    .attr("cy", function(d) { return  ((-d.Y)*multiplier) % 2 < 1 ? 0 : 1000; })
+		    .attr("cx", function(d) { return  xScale(d.X); })
+		    .attr("cy", function(d) { return  10; })
 		    .attr("r", function(d) { return 0;})
 		  	.transition()
-		  	.duration(function(d,i){return Math.min(9000, 3000 + i*200)})//function(d){return d.X*50;})
+		  	.duration(function(d,i){return Math.min(9000, 30 + i*20)})//function(d){return d.X*50;})
 			.delay(function(d,i){	
-				return getDelay(d,i);
-				})
+				return i*200;				})
 			.attr("fill",function(d,i){return color(d.zip);})
-		    .attr("cx", function(d) { return xScale(d.X); })
 		    .attr("cy", function(d) { return yScale(d.Y); })
-		    .attr("r", function(d) { return 2);});
+		    .attr("r", function(d) { return 5;});
 
 		if(window.location.hash.indexOf("timeline")>=0){
 			drawLine(rows, firstYear);
@@ -164,6 +166,5 @@ var drawLine = function(rows, firstYear){
                  .text(rows[rows.length-1].Orig_Iss_D.split('/')[0]);
 
 };
-
 
 setupMap();
